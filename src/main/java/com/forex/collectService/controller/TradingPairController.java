895c84e680aa072d7fc1ce7ca386e105cde7ac13
FrancisAO <Francis.Opoku@gmx.de>
2017-12-10 11:51:47 +0100

    Request Mapping 'collectTP' from TraidingPairController returns String for POST.

--- src/main/java/com/forex/collectService/controller/TradingPairController.java
+++ src/main/java/com/forex/collectService/controller/TradingPairController.java
@@ -5,6 +5,8 @@ import com.forex.collectService.model.AskBidQueryParams;
 import com.forex.collectService.model.CurrencyPair;
 import com.forex.collectService.repository.QueryExecuter;
 import com.forex.collectService.validator.AskBidQueryParamValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,54 +24,58 @@ import java.util.Map;
 public class TradingPairController {
 
 
-	@Autowired
-	private AskBidQueryParamValidator askBidQueryParamValidator;
-	@Autowired
-	private QueryExecuter queryExecuter;
-	private DateTimeFormatter dateTimeFormatter;
+    @Autowired
+    private AskBidQueryParamValidator askBidQueryParamValidator;
+    @Autowired
+    private QueryExecuter queryExecuter;
+    private DateTimeFormatter dateTimeFormatter;
+    Logger LOG = LoggerFactory.getLogger(TradingPairController.class);
 
-	@RequestMapping(path="/collectTP", method = RequestMethod.POST)
-	public void storeTraidingPairs(@RequestParam Map<String, String> queryParameters){
-		if(askBidQueryParamValidator.valid(queryParameters)){
-			insertQueryParametersInDatabase(queryParameters);
-		}
-	}
+    @RequestMapping(path = "/collectTP", method = RequestMethod.POST)
+    public String storeTraidingPairs(@RequestParam Map<String, String> queryParameters) {
+        if (!askBidQueryParamValidator.valid(queryParameters)) {
+            return "Fail :-(";
+        }
+        insertQueryParametersInDatabase(queryParameters);
+        return "Success :-)";
 
-	private void insertQueryParametersInDatabase(@RequestParam Map<String, String> queryParameters) {
-		CurrencyPair cp = extractCurrencyPair(queryParameters);
-		Double sell = extractNumber(AskBidQueryParams.SELL_PRICE, queryParameters);
-		Double buy = extractNumber(AskBidQueryParams.BUY_PRICE, queryParameters);
-		OffsetDateTime offsetDateTime = extractTimestamp(queryParameters);
-		String serviceId = extractServiceId(queryParameters);
+    }
 
-		queryExecuter.insertAskBid(cp, sell, buy, offsetDateTime, serviceId);
-	}
+    private void insertQueryParametersInDatabase(@RequestParam Map<String, String> queryParameters) {
+        CurrencyPair cp = extractCurrencyPair(queryParameters);
+        Double sell = extractNumber(AskBidQueryParams.SELL_PRICE, queryParameters);
+        Double buy = extractNumber(AskBidQueryParams.BUY_PRICE, queryParameters);
+        OffsetDateTime offsetDateTime = extractTimestamp(queryParameters);
+        String serviceId = extractServiceId(queryParameters);
 
-	private String extractServiceId(Map<String, String> queryParameters) {
-		return queryParameters.get(AskBidQueryParams.SERVICE_ID);
-	}
+        queryExecuter.insertAskBid(cp, sell, buy, offsetDateTime, serviceId);
+    }
 
-	private OffsetDateTime extractTimestamp(Map<String, String> queryParameters) {
-		String timeInMilliSecondsString = queryParameters.get(AskBidQueryParams.TIMESTAMP);
-		Long timeInMilliSeconds = Long.valueOf(timeInMilliSecondsString);
-		Instant timeInstant = Instant.ofEpochMilli(timeInMilliSeconds);
-		return OffsetDateTime.ofInstant(timeInstant, ZoneId.systemDefault());
-	}
+    private String extractServiceId(Map<String, String> queryParameters) {
+        return queryParameters.get(AskBidQueryParams.SERVICE_ID);
+    }
 
-	private Double extractNumber(String position, Map<String, String> queryParameters) {
-		String value = queryParameters.get(position);
-		return Double.valueOf(value);
-	}
+    private OffsetDateTime extractTimestamp(Map<String, String> queryParameters) {
+        String timeInMilliSecondsString = queryParameters.get(AskBidQueryParams.TIMESTAMP);
+        Long timeInMilliSeconds = Long.valueOf(timeInMilliSecondsString);
+        Instant timeInstant = Instant.ofEpochMilli(timeInMilliSeconds);
+        return OffsetDateTime.ofInstant(timeInstant, ZoneId.systemDefault());
+    }
 
-	private CurrencyPair extractCurrencyPair(Map<String, String> queryParams) {
-		String currencyPair = queryParams.get(AskBidQueryParams.CURRENCY_PAIR);
-		return QueryParamValueExtractor.extractCurrencyPair(currencyPair);
-	}
+    private Double extractNumber(String position, Map<String, String> queryParameters) {
+        String value = queryParameters.get(position);
+        return Double.valueOf(value);
+    }
 
-	@PostConstruct
-	private void initDateTimeFormatter(){
-		dateTimeFormatter = DateTimeFormatter.ofPattern("");
-	}
+    private CurrencyPair extractCurrencyPair(Map<String, String> queryParams) {
+        String currencyPair = queryParams.get(AskBidQueryParams.CURRENCY_PAIR);
+        return QueryParamValueExtractor.extractCurrencyPair(currencyPair);
+    }
+
+    @PostConstruct
+    private void initDateTimeFormatter() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("");
+    }
 
 
 }
